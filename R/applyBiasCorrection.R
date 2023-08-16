20921f42cc465a4de6ee1d80720ebced997c99ad
Koen Van den Berge <KVande14@its.jnj.com>
2023-08-16 15:17:12 +0200

    fix bug in calculating fitted values in voomCLR

--- R/applyBiasCorrection.R
+++ R/applyBiasCorrection.R
@@ -3,10 +3,15 @@
 #' @name applyBiasCorrection
 #' @export
 applyBiasCorrection <- function(fit){
+  par(mfrow=c(3,3), a = 1, b = 1, brewer.n = 8, brewer.name = "Dark2", 
+      cex.lab = 1, cex.main = 1.2, cex.axis = 1, mar = c(2.5, 2.5, 
+                                                         1.6, 1.1), 
+      mgp = c(1.5, 0.5, 0))
   n <- nrow(fit$design)
   biasCorrCoef <- apply(fit$coefficients, 2, function(x){
     x - .getMode(x, n)
   })
   fit$coefficients <- biasCorrCoef
+  par(mfrow=c(1,1))
   return(fit)
 }
--- R/voomCLR.R
+++ R/voomCLR.R
@@ -1,27 +1,4 @@
-## example
-# set.seed(495212344)
-# n <- 40 # sample size
-# P <- 10 # number of cell types
-# mu0 <- rnbinom(n=P, size=1/2, mu=400)
-# mu0 # absolute counts in group 0
-# beta <- rlnorm(n=P, meanlog = 0, sdlog=2) * # these are log-fold-changes
-#   rbinom(n=P, size=1, prob=.15) *
-#   sample(c(-1,1), size=P, replace=TRUE) # fold change on log scale
-# mu1 <- exp(beta) * mu0 # because we want log(mu2/mu1) = beta
-# relAbundances <- data.frame(g0=mu0/sum(mu0),
-#                             g1=mu1/sum(mu1)) # relative abundance of absolute count
-# # relative abundance information (observed data in typical experiment)
-# Y0 <- rmultinom(n=10, size=1e4, prob=relAbundances$g0)
-# Y1 <- rmultinom(n=10, size=1e4, prob=relAbundances$g1)
-# Y <- cbind(Y0, Y1)
-# group <- factor(rep(0:1, each=10))
-# design <- model.matrix(~group)
-# v <- voomCLR(counts = Y,
-#              design = design,
-#              lib.size = NULL)
-# fit <- lmFit(v, design)
-# fit <- applyBiasCorrection(fit)
-# fit <- eBayes(fit)
+
 
 ## for interactively running the code:
 # counts=Y
@@ -172,7 +149,7 @@ voomCLR <- function(counts,
                     block=NULL,
                     correlation=NULL,
                     weights=NULL,
-                    span=0.5,
+                    span=0.8,
                     plot=FALSE,
                     save.plot=FALSE,
                     varCalc="empirical")
@@ -272,7 +249,8 @@ voomCLR <- function(counts,
   # fitted.cpm <- 2^fitted.values
   fittedRatio <- exp(fitted.values)
   # fitted.count <- 1e-6 * t(t(fitted.cpm)*(lib.size+1))
-  fitted.count <- fittedRatio * geoMeans
+  # fitted.count <- fittedRatio * geoMeans
+  fitted.count <- t(apply(fittedRatio,1,"*",geoMeans))
   # fitted.logcount <- log2(fitted.count)
   fitted.logcount <- log(fitted.count)
   
@@ -313,12 +291,42 @@ voomCLR <- function(counts,
 
 
 
+# ## example
+# set.seed(495212344)
+# n <- 40 # sample size
+# P <- 10 # number of cell types
+# mu0 <- rnbinom(n=P, size=1/2, mu=400)
+# mu0 # absolute counts in group 0
+# beta <- rlnorm(n=P, meanlog = 0, sdlog=2) * # these are log-fold-changes
+#   rbinom(n=P, size=1, prob=.15) *
+#   sample(c(-1,1), size=P, replace=TRUE) # fold change on log scale
+# mu1 <- exp(beta) * mu0 # because we want log(mu2/mu1) = beta
+# relAbundances <- data.frame(g0=mu0/sum(mu0),
+#                             g1=mu1/sum(mu1)) # relative abundance of absolute count
+# # relative abundance information (observed data in typical experiment)
+# Y0 <- rmultinom(n=10, size=1e4, prob=relAbundances$g0)
+# Y1 <- rmultinom(n=10, size=1e4, prob=relAbundances$g1)
+# Y <- cbind(Y0, Y1)
+# rownames(Y) <- paste0("celltype",1:10)
+# colnames(Y) <- paste0("sample",1:20)
+# group <- factor(rep(0:1, each=10))
+# design <- model.matrix(~group)
+# v <- voomCLR(counts = Y,
+#              design = design,
+#              lib.size = NULL,
+#              plot = TRUE)
+# fit <- lmFit(v, design)
+# fit <- applyBiasCorrection(fit)
+# fit <- eBayes(fit)
 
-#### workflow
-## requires two objects:
-##  - design matrix as constructed using model.matrix
-##  - count matrix where rows are populations and columns are samples
+
+
+# #### workflow
+# ## requires two objects:
+# ##  - design matrix as constructed using model.matrix
+# ##  - count matrix where rows are populations and columns are samples
 # library(limma)
+# library(voomCLR)
 # v <- voomCLR(counts = countMatrix,
 #              design = design,
 #              lib.size = NULL)
@@ -326,7 +334,7 @@ voomCLR <- function(counts,
 # plotBeta(fit)
 # fit <- applyBiasCorrection(fit)
 # fit <- eBayes(fit)
-# testing is custom: fill in based on hypothesis of interest.
+# # testing is custom: fill in based on hypothesis of interest.
 # tt <- topTable(fit, ....)
 
 
